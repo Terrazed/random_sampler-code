954a440b0a3ffea0192aeea6b7c2e4a7773357f3
trz <simon.donnetmonay@gmail.com>
2025-06-05 17:45:35 +0200

    added random sleep time between audio

--- main/test02.c
+++ main/test02.c
@@ -4,129 +4,98 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
+#include <math.h>
 #include <stdint.h>
+#include <sys/_timeval.h>
 #include "driver/i2c_types.h"
+#include "esp_attr.h"
 #include "esp_log.h"
+#include "esp_log_level.h"
 #include "esp_private/gpio.h"
 #include "esp_err.h"
 #include "soc/gpio_num.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <sys/time.h>
+#include "esp_random.h"
+#include "bootloader_random.h"
 
 #include "audio_pipeline.h"
-
 #include "esp_sleep.h"
 
 
-//#define GPIO_POWER_SD GPIO_NUM_5
-//#define GPIO_POWER_AMPLIFIER GPIO_NUM_4
+static RTC_DATA_ATTR time_t current_time;
+
+
+uint64_t get_random_time_us(){
+
+    //get random number
+    bootloader_random_enable();
+    uint32_t u1 = esp_random();
+    uint32_t u2 = esp_random();
+    bootloader_random_disable();
+
+    //convert them to float
+    float uf1 = (float)u1/(float)UINT32_MAX;
+    if(uf1 == 0 || uf1 == 1){
+        uf1 = 0.5;
+    }
+    float uf2 = (float)u2/(float)UINT32_MAX;
+    if(uf2 == 0|| uf2 == 1){
+        uf2 = 0.5;
+    }
+
+    //normal distribution parameters
+    const float STD_DEV = 90 * 1000000; // 1.5min
+    const float MEAN = 300 * 1000000; // 5min
+
+    //box-muller transform
+    float z0 = sqrtf(-2.0f * logf(uf1)) * cosf(2.0f * M_PI * uf2) * STD_DEV + MEAN;
+
+    //limits
+    if(z0 <= 0 || z0 >= 600000000){
+        z0 = MEAN;
+    }
+
+    //convert to uint32_t
+    uint64_t output = z0;
+
+    //ESP_LOGI("RANDOM", "output : %u", (uint32_t)output);
+
+
+
+
+    return output;
+}
+
 
 void app_main(void)
 {
     ESP_LOGI("APP","Program starting!");
-
     esp_err_t err;
 
-    // GPIO ---------------------------------------------------------------------
 
-    //ESP_LOGI("GPIO","configuring GPIO_POWER_SD");
-    //err = gpio_func_sel(GPIO_POWER_SD, 1);
-    //if(err != ESP_OK) {
-    //    ESP_LOGE("GPIO","Failed to configure GPIO_POWER_SD");
-    //    return;
-    //}
-    //err = gpio_set_direction(GPIO_POWER_SD, GPIO_MODE_OUTPUT);
-    //if(err != ESP_OK) {
-    //    ESP_LOGE("GPIO","Failed to set direction for GPIO_POWER_SD");
-    //    return;
-    //}
-    //err = gpio_set_level(GPIO_POWER_SD, 1);
-    //if(err != ESP_OK) {
-    //    ESP_LOGE("GPIO","Failed to set level for GPIO_POWER_SD");
-    //    return;
-    //}
-    //ESP_LOGI("GPIO","done configuring GPIO_POWER_SD");
+    time(&current_time);
+    struct tm *temp_current_time = localtime(&current_time);
+    ESP_LOGW("time", "current time: %uh %umin %usec, %u/%u/%u", temp_current_time->tm_hour, temp_current_time->tm_min, temp_current_time->tm_sec, temp_current_time->tm_mday, temp_current_time->tm_mon, temp_current_time->tm_year);
 
-    //ESP_LOGI("GPIO","configuring GPIO_POWER_AMPLIFIER");
-    //err = gpio_func_sel(GPIO_POWER_AMPLIFIER, 1);
-    //if(err != ESP_OK) {
-    //    ESP_LOGE("GPIO","Failed to configure GPIO_POWER_AMPLIFIER");
-    //    return;
-    //}
-    //err = gpio_set_direction(GPIO_POWER_AMPLIFIER, GPIO_MODE_OUTPUT);
-    //if(err != ESP_OK) {
-    //    ESP_LOGE("GPIO","Failed to set direction for GPIO_POWER_AMPLIFIER");
-    //    return;
-    //}
-    //err = gpio_set_level(GPIO_POWER_AMPLIFIER, 1);
-    //if(err != ESP_OK) {
-    //    ESP_LOGE("GPIO","Failed to set level for GPIO_POWER_AMPLIFIER");
-    //    return;
-    //}
-    //ESP_LOGI("GPIO","done configuring GPIO_POWER_AMPLIFIER");
 
-    // I2C ----------------------------------------------------------------------
 
-    //i2c_master_bus_handle_t bus_handle;
-    //i2c_master_dev_handle_t dev_handle;
-
-    //i2c_master_init(&bus_handle, &dev_handle);
-    //ESP_LOGI("I2C","done initializing I2C bus");
-
-    //tfa9879_register_write_byte(dev_handle, 0x00, 0x0008);
-    //tfa9879_register_write_byte(dev_handle, 0x00, 0x0009);
-    //tfa9879_register_write_byte(dev_handle, 0x01, 0x08D0);
-    //tfa9879_register_write_byte(dev_handle, 0x13, 0x1080);
-
-    //uint8_t data[2];
-    //for(int i = 0; i<=0x15; i++){
-    //    ESP_ERROR_CHECK(tfa9879_register_read(dev_handle, i, data, 0x02));
-    //    ESP_LOGI("I2C","reg 0x%02x value 0x%02x%02x", i, data[0], data[1]);
+    //for(uint16_t i = 0; i<1000; i++){
+    //    get_random_time_us();
     //}
 
-    //vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-    // I2S ----------------------------------------------------------------------
-
-    //ESP_LOGI("I2S", "i2s config");
-    //i2s_chan_handle_t i2s_chan_handle;
-    //i2s_init(&i2s_chan_handle);
-    //ESP_LOGI("I2S", "i2s write task");
-    //xTaskCreate(i2s_write_task, "i2s_write_task", 4096, &i2s_chan_handle, 5, NULL); //play the audio in loop
 
 
 
-    // SD card ------------------------------------------------------------------
-
-    //sdcard_init();
-
-
-    // MAIN LOOP ----------------------------------------------------------------
-
-    //s_example_read_file("/sdcard/samples/sample_0.wav");
-
-/*
-    FILE *f = NULL;
-    sdcard_open_file(f, "/sdcard/samples/sample_0.wav", "r");
-
-    char buffer[16+1];
-
-    for (int j = 0; j<32; j++){
-        sdcard_read_bytes(f, buffer, 16, true);
-
-        ESP_LOGI("read", "%x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7], buffer[8], buffer[9], buffer[10], buffer[11], buffer[12], buffer[13], buffer[14], buffer[15]);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+    err = audio_pipeline_init();
+    if(err != ESP_OK){
+        ESP_LOGE("MAIN", "Failed to initialize pipeline!");
+        return;
     }
-
-    sdcard_close_file(f);
-*/
-    //esp_log_level_set("*", ESP_LOG_ERROR);
-
-
-    audio_pipeline_init();
     //vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-    static RTC_DATA_ATTR int test = 0;
+
 
     while (true) {
 
@@ -142,11 +111,19 @@ void app_main(void)
         //audio_pipeline_play_file("/sdcard/samples/veridis_quo(3).wav");
         audio_pipeline_play_file("/sdcard/samples/sample_0.wav");
 
-        ESP_LOGI("test", "bloup %u", test++);
+        uint64_t time_to_sleep_us;
 
+        struct tm *temp_current_time = localtime(&current_time);
+        if(temp_current_time->tm_hour < 12){
 
-        ESP_LOGI("DEEPSLEEP", "sleep for 1s...");
-        ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(2*1000000));
+            time_to_sleep_us = get_random_time_us();
+        }
+        else {
+            time_to_sleep_us = 43200000000; //12h
+        }
+
+        ESP_LOGI("DEEPSLEEP", "sleep for %u seconds...", (uint32_t)((uint64_t)time_to_sleep_us/(uint64_t)1000000));
+        ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(time_to_sleep_us));
         esp_deep_sleep_start();
         //audio_pipeline_play_file("/sdcard/samples/veridis_quo_auto.wav");
         //audio_pipeline_play_file("/sdcard/samples/veridis_quo_24bit_auto.wav");
